9df95bc5f8cfadfc98412cbb80bc0e7bead9800f
Tom Collier <collier@apartmentlist.com>
2021-01-07 18:04:38 -0800

    Move all fake days to Y2K

--- 00/main.ts
+++
@@ -1,7 +0,0 @@
-const loadData = (): string[] => ["Hello", "World!"]
-
-const part1Result = (words: string[]): string => words[0]
-
-const part2Result = (words: string[]): string => words[1]
-
-export { loadData, part1Result, part2Result }
