bd9ee640c4ff266180af15a9b3dd3a252b2825a5
Tom Collier <tcollier@squareup.com>
2022-12-02 18:12:21 -0800

    Improve 2022 day 1 solution

--- 2022/01/main.rb
+++ 2022/01/main.rb
@@ -1,15 +1,9 @@
 #!/usr/bin/env ruby
 
-ELF_CALORIES = [0]
-elf_index = 0
-$<.each do |line|
-  chomped = line.chomp
-  if chomped == ""
-    elf_index += 1
-    ELF_CALORIES << 0
-  else
-    ELF_CALORIES[elf_index] += Integer(chomped)
-  end
-end
-puts ELF_CALORIES.max
-puts ELF_CALORIES.sort.reverse[0..2].sum
+$<.chunk { _1 != "\n" }
+  .select(&:first)
+  .map(&:last)
+  .map { _1.sum(&:to_i) }
+  .sort
+  .tap { p _1.last }
+  .tap { p _1.last(3).sum }
