f957b118d3a2c55c3f1e3c5d9ad36eb997bcc7c1
Tom Collier <tcollier@squareup.com>
2022-12-04 10:13:22 -0800

    Simplify imag part

--- 2022/04/main.rb
+++ 2022/04/main.rb
@@ -2,4 +2,4 @@
 
 puts $<.map { _1.gsub(/(\d+)-(\d+)/, '(\1..\2).to_a') }
   .map { _1.split(',').map(&method(:eval)) }
-  .sum { |(a, b)| [a, b].uniq.count(a & b) + (a & b).map { _1 / _1 }.uniq.first.to_i * 1i }
+  .sum { |(a, b)| [a, b].uniq.count(a & b) + (a & b).map { 1i }.uniq.sum }
