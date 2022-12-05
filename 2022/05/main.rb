d2f1e44eb91b0c694f37a14a2de365771b5e893d
Tom Collier <tcollier@squareup.com>
2022-12-04 22:08:09 -0800

    Improve 2022 day 5 solution

--- 2022/05/main.rb
+++ 2022/05/main.rb
@@ -1,29 +1,24 @@
 #!/usr/bin/env ruby
 
 input = $<.map(&:chomp)
-NUM_STACKS = 9
-STACKS_PT1 = Array.new(NUM_STACKS) { Array.new }
-STACKS_PT2 = Array.new(NUM_STACKS) { Array.new }
-tmp = []
-while (next_row = input.shift.gsub(/\[|\]|/, '').gsub(/    /, ',').gsub(/ /, ','))[1] != '1' do
-  tmp.unshift(next_row.split(',').map { _1 == '' ? nil : _1 })
-end
-while line = tmp.shift
-  NUM_STACKS.times do |index|
-    STACKS_PT1[index].push(line[index]) unless line[index].nil?
-    STACKS_PT2[index].push(line[index]) unless line[index].nil?
+label_line_num = input.index { _1[1] == '1' }
+label_map = input[label_line_num].split(' ').map.with_index.with_object({}) { |(l, i), h| h[l] = i }
+stacks_1 = Array.new(label_map.count) { Array.new }
+stacks_2 = Array.new(label_map.count) { Array.new }
+(label_line_num - 1).downto(0) do |line_num|
+  row = input[line_num].gsub(/\[|\]|/, '').gsub(/    /, ',').gsub(/ /, ',').split(',').map { _1 == '' ? nil : _1 }
+  label_map.count.times do |index|
+    stacks_1[index].push(row[index]) unless row[index].nil?
+    stacks_2[index].push(row[index]) unless row[index].nil?
   end
 end
-
-input.shift
-
-input.map do |line|
-  match = line.match(/move (\d+) from (\d+) to (\d+)/)
-  num_crates, src, dest = [match[1].to_i, match[2].to_i - 1, match[3].to_i - 1]
-  num_crates.times { STACKS_PT1[dest].push(STACKS_PT1[src].pop) }
-  num_crates.times { tmp.push(STACKS_PT2[src].pop) }
-  num_crates.times { STACKS_PT2[dest].push(tmp.pop) }
+input[(label_line_num + 2)..-1].map do |line|
+  match = line.match(/move (\d+) from (.+) to (.+)/)
+  num_crates, src, dest = [match[1].to_i, match[2], match[3]]
+  num_crates.times { stacks_1[label_map[dest]].push(stacks_1[label_map[src]].pop) }
+  tmp = num_crates.times.map { stacks_2[label_map[src]].pop }
+  num_crates.times { stacks_2[label_map[dest]].push(tmp.pop) }
 end
 
-puts STACKS_PT1.map { _1.pop }.join
-puts STACKS_PT2.map { _1.pop }.join
+puts stacks_1.map { _1.pop }.join
+puts stacks_2.map { _1.pop }.join
