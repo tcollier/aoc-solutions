154bfdf00100c88b88181ba56759d8988f1b801f
Tom Collier <tcollier@squareup.com>
2022-12-08 08:49:07 -0800

    Improve 2022 day 8

--- 2022/08/main.rb
+++ 2022/08/main.rb
@@ -1,67 +1,32 @@
 #!/usr/bin/env ruby
 
+HEIGHTS = $<.map { _1.chomp.chars.map(&:to_i) }
 
-HEIGHTS = $<.map { |line| line.chomp.chars.map(&:to_i) }
-VISIBLE = Array.new(HEIGHTS.length) { Array.new(HEIGHTS[0].length) { false } }
-# SCENIC_SCORES = Array.new(HEIGHTS.length) { Array.new(HEIGHTS[0].length) { 0 } }
-
-HEIGHTS.length.times do |row|
-  max_height = -1
-  HEIGHTS[row].length.times do |col|
-    if HEIGHTS[row][col] > max_height
-      VISIBLE[row][col] = true
-      max_height = HEIGHTS[row][col]
-    end
-  end
-  max_height = -1
-  (HEIGHTS[row].length - 1).downto(0).each do |col|
-    if HEIGHTS[row][col] > max_height
-      VISIBLE[row][col] = true
-      max_height = HEIGHTS[row][col]
-    end
-  end
-end
-HEIGHTS[0].length.times do |col|
-  max_height = -1
-  HEIGHTS.length.times do |row|
-    if HEIGHTS[row][col] > max_height
-      VISIBLE[row][col] = true
-      max_height = HEIGHTS[row][col]
-    end
-  end
-  max_height = -1
-  (HEIGHTS.length - 1).downto(0).each do |row|
-    if HEIGHTS[row][col] > max_height
-      VISIBLE[row][col] = true
-      max_height = HEIGHTS[row][col]
-    end
-  end
-end
-
-puts VISIBLE.sum { |line| line.count { _1 } }
-
+num_visible = (HEIGHTS.length - 1) * 2 + (HEIGHTS[0].length - 1) * 2
 max_scenic_score = 0
+
+vs = -> (height, rows, cols) do
+  shorter = rows.map { |row| cols.map { |col| [row, col] } }.flatten(1).take_while do |pos|
+    HEIGHTS[pos.first][pos.last] < height
+  end
+  num_trees = [rows.count, cols.count].max
+  [shorter.length == num_trees, [shorter.length + 1, num_trees].min]
+end
+
 (1..(HEIGHTS.length - 2)).each do |row|
   (1..(HEIGHTS[row].length - 2)).each do |col|
-    up_score = down_score = left_score = right_score = 0
-    (row - 1).downto(0).each do |view_row|
-      up_score += 1
-      break if HEIGHTS[view_row][col] >= HEIGHTS[row][col]
+    result = [
+      vs.call(HEIGHTS[row][col], (row - 1).downto(0), [col]),
+      vs.call(HEIGHTS[row][col], (row + 1).upto(HEIGHTS.length - 1), [col]),
+      vs.call(HEIGHTS[row][col], [row], (col - 1).downto(0)),
+      vs.call(HEIGHTS[row][col], [row], (col + 1).upto(HEIGHTS[row].length - 1)),
+    ].reduce([false, 1]) do |acc, item|
+      [acc.first || item.first, acc.last * item.last]
     end
-    (row + 1).upto(HEIGHTS.length - 1).each do |view_row|
-      down_score += 1
-      break if HEIGHTS[view_row][col] >= HEIGHTS[row][col]
-    end
-    (col - 1).downto(0).each do |view_col|
-      left_score += 1
-      break if HEIGHTS[row][view_col] >= HEIGHTS[row][col]
-    end
-    (col + 1).upto(HEIGHTS[row].length - 1).each do |view_col|
-      right_score += 1
-      break if HEIGHTS[row][view_col] >= HEIGHTS[row][col]
-    end
-    scenic_score = up_score * left_score * down_score * right_score
-    max_scenic_score = scenic_score if scenic_score > max_scenic_score
+    num_visible += 1 if result.first
+    max_scenic_score = result.last if result.last > max_scenic_score
   end
 end
+
+puts num_visible
 puts max_scenic_score
