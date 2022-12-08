955843ae5d89a6bf07a20b49d162d9580d9f3845
Tom Collier <tcollier@squareup.com>
2022-12-08 09:45:23 -0800

    Improve 2022 day 8

--- 2022/08/main.rb
+++ 2022/08/main.rb
@@ -2,31 +2,25 @@
 
 HEIGHTS = $<.map { _1.chomp.chars.map(&:to_i) }
 
-num_visible = (HEIGHTS.length - 1) * 2 + (HEIGHTS[0].length - 1) * 2
-max_scenic_score = 0
-
-vs = -> (height, rows, cols) do
-  shorter = rows.map { |row| cols.map { |col| [row, col] } }.flatten(1).take_while do |pos|
-    HEIGHTS[pos.first][pos.last] < height
-  end
+def vs(height, rows, cols)
+  num_shorter = rows.map { |row| cols.map { |col| HEIGHTS[row][col] } }.flatten.take_while { _1 < height }.length
   num_trees = [rows.count, cols.count].max
-  [shorter.length == num_trees, [shorter.length + 1, num_trees].min]
+  [num_shorter < num_trees ? 0 : 1, [num_shorter + 1, num_trees].min]
 end
 
-(1..(HEIGHTS.length - 2)).each do |row|
-  (1..(HEIGHTS[row].length - 2)).each do |col|
-    result = [
-      vs.call(HEIGHTS[row][col], (row - 1).downto(0), [col]),
-      vs.call(HEIGHTS[row][col], (row + 1).upto(HEIGHTS.length - 1), [col]),
-      vs.call(HEIGHTS[row][col], [row], (col - 1).downto(0)),
-      vs.call(HEIGHTS[row][col], [row], (col + 1).upto(HEIGHTS[row].length - 1)),
-    ].reduce([false, 1]) do |acc, item|
-      [acc.first || item.first, acc.last * item.last]
+answer = (1..(HEIGHTS.length - 2)).map do |row|
+  (1..(HEIGHTS[row].length - 2)).map do |col|
+    [
+      vs(HEIGHTS[row][col], (row - 1).downto(0), [col]),
+      vs(HEIGHTS[row][col], (row + 1).upto(HEIGHTS.length - 1), [col]),
+      vs(HEIGHTS[row][col], [row], (col - 1).downto(0)),
+      vs(HEIGHTS[row][col], [row], (col + 1).upto(HEIGHTS[row].length - 1)),
+    ].reduce([0, 1]) do |acc, item|
+      [[acc.first, item.first].max, acc.last * item.last]
     end
-    num_visible += 1 if result.first
-    max_scenic_score = result.last if result.last > max_scenic_score
   end
+end.flatten(1).reduce([(HEIGHTS.length - 1) * 2 + (HEIGHTS[0].length - 1) * 2, 0]) do |acc, item|
+  [acc.first + item.first, [acc.last, item.last].max]
 end
 
-puts num_visible
-puts max_scenic_score
+puts answer.join("\n")
